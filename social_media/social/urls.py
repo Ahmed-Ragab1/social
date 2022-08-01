e6c3231d998919eb72c0f8f1dcd8fc7499bed46f
PROFabdalla‏ <abdallaamer101@gmail.com>
2022-08-01 15:05:49 +0200

    adding about us page

--- social_media/social/urls.py
+++ social_media/social/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import home
+from .views import home,aboutus
 
 
 
 urlpatterns = [
     path('home/',home,name="home"),
+    path('aboutus/',aboutus,name="aboutus"),
 ]
--- social_media/social/views.py
+++ social_media/social/views.py
@@ -8,3 +8,7 @@ from django.shortcuts import render
 def home(request):
     return render(request,'home.html')
 
+def aboutus(request):
+    return render(request,'aboutus.html')
+
+
