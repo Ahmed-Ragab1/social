92de96f20a43e4d36739dbbc0af0cf2fd13f43f3
PROFabdalla‏ <abdallaamer101@gmail.com>
2022-08-01 16:23:24 +0200

    adding about us page image

--- social_media/social/urls.py
+++ social_media/social/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from .views import home,aboutus
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 
 urlpatterns = [
     path('home/',home,name="home"),
     path('aboutus/',aboutus,name="aboutus"),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
